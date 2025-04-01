1823ac8fe78626c5346e6bb3aebcc1d51f4b5900
corneanu <constantin.corneanu@foodomarket.com>
2025-04-01 13:59:00 +0300

    compoents in place

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Header />
+  <div class="container">
+      <Balance />
+      <IncomeExpences />
+      <TranzactionList />
+      <AddTransaction />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpences from './components/IncomeExpense.vue';
+import TranzactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+
+export default {
+  components:{
+    Header,
+    Balance,
+    IncomeExpences,
+    TranzactionList,
+    AddTransaction
+},
+
+};
+</script>
