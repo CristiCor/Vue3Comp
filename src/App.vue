081393ad3fdf03348fd6c25103156353dd8e9cd6
corneanu <constantin.corneanu@foodomarket.com>
2025-04-02 01:49:45 +0300

    transaction list, total

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <Header />
   <div class="container">
-      <Balance />
+      <Balance :total="total"/>
       <IncomeExpences />
-      <TranzactionList />
+      <TranzactionList :transactions="transactions"/>
       <AddTransaction />
   </div>
 </template>
 
-<script>
+<script setup>
 import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
 import IncomeExpences from './components/IncomeExpense.vue';
 import TranzactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
 
-export default {
-  components:{
-    Header,
-    Balance,
-    IncomeExpences,
-    TranzactionList,
-    AddTransaction
-},
+import {ref, computed } from 'vue';
 
-};
+const transactions = ref([
+    {id:1, text: "Flower", amount: -19.99},
+    {id:2, text: "Salary", amount: 11333.99},
+    {id:3, text: "Book", amount: -10.99},
+    {id:4, text: "Revenue", amount: 22000.00},
+]);
+
+const total = computed(()=>{
+  return transactions.value.reduce((acc, transaction)=> {
+    return acc + transaction.amount; 
+  }, 0);
+});
 </script>
