d27419a4eff56a98f6accc73ed1d59ccb5a49baf
corneanu <constantin.corneanu@foodomarket.com>
2025-04-02 03:03:41 +0300

    Emit transaction add, delete

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <Header />
   <div class="container">
-      <Balance :total="total"/>
-      <IncomeExpences />
-      <TranzactionList :transactions="transactions"/>
-      <AddTransaction />
+      <Balance :total="+total"/>
+      <IncomeExpences :income="+income" :expenses="+expenses"/>
+      <TranzactionList :transactions="transactions" @transactionDeleted="handleTransactionDeleted"/>
+      <AddTransaction @transactionSubmitted="handleTransactionSubmitted"/>
   </div>
 </template>
 
@@ -15,18 +15,60 @@ import IncomeExpences from './components/IncomeExpense.vue';
 import TranzactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
 
+import {useToast} from 'vue-toastification';
+
 import {ref, computed } from 'vue';
 
+const toast = useToast();
+
 const transactions = ref([
-    {id:1, text: "Flower", amount: -19.99},
-    {id:2, text: "Salary", amount: 11333.99},
-    {id:3, text: "Book", amount: -10.99},
-    {id:4, text: "Revenue", amount: 22000.00},
+    {id:1, text: "Flower", amount: parseFloat("-19.00")},
+    {id:2, text: "Salary", amount: parseFloat("1000.00")},
+    {id:3, text: "Book", amount: parseFloat("-10.00")},
+    {id:4, text: "Revenue", amount: parseFloat("8000.00")},
 ]);
 
+//Get total
 const total = computed(()=>{
-  return transactions.value.reduce((acc, transaction)=> {
-    return acc + transaction.amount; 
-  }, 0);
+  return transactions.value.reduce((acc, transaction)=> acc + transaction.amount, 0);
 });
+
+//get Income
+const income = computed(()=>{
+  return transactions.value
+  .filter((transaction)=> transaction.amount > 0 )
+  .reduce((acc, transaction) => acc + transaction.amount, 0)
+  .toFixed(2);
+});
+
+//get expenses
+const expenses = computed(()=>{
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0 )
+    .reduce((acc, transaction) => acc + transaction.amount, 0)
+    .toFixed(2);
+});
+
+//Add transaction
+const handleTransactionSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: generateUniqueId(),
+    text: transactionData.text,
+    amount: transactionData.amount
+  });
+
+  toast.success('Transaction added !');
+};
+
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 1000000);
+};
+
+//Delete transaction
+const handleTransactionDeleted = (id) => {
+  transactions.value = transactions.value.filter((transaction) => transaction.id !== id);
+
+  toast.success('Transaction deleted !');
+}
+
 </script>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -6,7 +6,8 @@
             v-bind:key="transaction.id"
             :class="transaction.amount < 0 ? 'minus' : 'plus'"
             >
-          {{ transaction.text }}<span>{{ transaction.amount }} Euro</span><button class="delete-btn">x</button>
+          {{ transaction.text }}<span>{{ transaction.amount }} Euro</span>
+          <button @click="deleteTransaction(transaction.id)" class="delete-btn">x</button>
         </li>
 
         <!-- <li class="minus">
@@ -21,10 +22,17 @@
 <script setup>
 import {defineProps} from 'vue';
 
+const emit = defineEmits(['transactionDeleted']);
+
 const props = defineProps({
     transactions: {
         type: Array,
         required: true
     }
-})
+});
+
+
+const deleteTransaction = (id) => {
+  emit('transactionDeleted', id);
+};
 </script>
